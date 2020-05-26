17ffd39f257a8b0f0404586c69770e28b0fb6bdd
Josegcb <Josegcb@gmail.com>
2020-05-26 09:41:34 -0400

    Recien Creado

---
+++ CRM.Mobile/CRM.Mobile/ViewModels/AboutViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Windows.Input;
+
+using Xamarin.Forms;
+
+namespace CRM.Mobile.ViewModels {
+    public class AboutViewModel : BaseViewModel {
+        public AboutViewModel() {
+            Title = "About";
+            OpenWebCommand = new Command(() => Device.OpenUri(new Uri("https://galac.com/")));
+        }
+
+        public ICommand OpenWebCommand { get; }
+    }
+}
---
+++ CRM.Mobile/CRM.Mobile/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+using CRM.Mobile.Models;
+
+namespace CRM.Mobile.ViewModels {
+    public class ItemDetailViewModel : BaseViewModel {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null) {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
